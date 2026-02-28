36f2edfc1a47a3c125597d295c49faeede19b125
arun <arunsindiri123@gmail.com>
2026-02-28 04:25:32 +0000

    removed every thing

--- video-upload-site/backend/routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const uploadController = require("../controllers/uploadController");
-
-router.post("/add", uploadController.addUpload);
-router.get("/all", uploadController.getAllUploads);
-
-module.exports = router;
